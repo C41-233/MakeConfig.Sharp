f8d61db91b184bc05d5be0dc44346421cd6486ba
C41 <c41hahaha@163.com>
2019-07-11 16:29:48 +0800

    load demo

--- Project/MakeConfig.Sharp/Program.cs
+++ Project/MakeConfig.Sharp/Program.cs
@@ -1,9 +1,43 @@
-﻿namespace MakeConfig.Sharp
+﻿using System;
+using System.IO;
+using OfficeOpenXml;
+
+namespace MakeConfig.Sharp
 {
-    internal class Program
+    internal static class Program
     {
+
         private static void Main(string[] args)
         {
+            using (var package = LoadExcelPackage(@"D:\Workspace\MakeConfig.Sharp\Test\Input\Item.xlsx"))
+            {
+                foreach (var sheet in package.Workbook.Worksheets)
+                {
+                    Console.WriteLine(sheet.Name);
+                }
+            }
         }
+
+        private static ExcelPackage LoadExcelPackage(string path)
+        {
+            ExcelPackage excelPackage = new ExcelPackage();
+            FileStream fileStream = null;
+            try
+            {
+                fileStream = File.OpenRead(path);
+            }
+            catch (IOException)
+            {
+                string tempFileName = Path.GetTempFileName();
+                File.Copy(path, tempFileName, true);
+                fileStream = File.OpenRead(tempFileName);
+            }
+            using (fileStream)
+            {
+                excelPackage.Load(fileStream);
+            }
+            return excelPackage;
+        }
+
     }
 }
